2d5a9b79ca334c5956ecb9f47794e16536e3282d
Thananon Sananrum <s6804062612234@email.kmutnb.ac.th>
2026-02-22 03:07:44 +0700

    lab8no3.c

--- lab8.c
+++ lab8.c
@@ -1,61 +1,57 @@
 #include <stdio.h>
-int checkscore(char std[], char keys[]);
-int checkhardest(char ans[][10], char keys[]);
-int no1 = 0, cur_lowest=10, lowest=10, hardest;
+#include <stdlib.h>
+#include <time.h>
+int random1to100();
+void printAr(int numAr[],int numArSize);
+int checkNum(int numAr[],int numArSize);
+int searchNum(int numAr[],int numArSize,int target);
+
 
 int main() {
-    int i, j;
-    char ans[8][10]={
-        	{'A','B','A','C','C','D','E','E','A','D'},//7
-            {'D','B','A','B','C','A','E','E','A','D'},//6
-            {'E','D','D','A','C','B','E','E','A','D'},//5
-            {'C','B','A','E','D','C','E','E','A','D'},//4
-            {'A','B','D','C','C','D','E','E','A','D'},//8
-            {'B','B','E','C','C','D','E','E','A','D'},//7
-            {'B','B','A','C','C','D','E','E','A','D'},//7
-            {'E','B','E','C','C','D','E','E','A','D'}};//7
+    int N,i;
+    int A[100];
+    scanf("%d",&N);
+    if (N>100) 
+        N=100;
+    srand(time(NULL));
+    for (i=0;i<N;i++)
+        A[i]=checkNum(A,i);
+       
+    printf("\n********\n");
+    printAr(A,N);
+    
+    return 0;
+}
+void printAr(int numAr[],int numArSize) {
+    int i;
+    for (i=0;i<numArSize;i++)
+        printf("%d ",numAr[i]);
+} 
 
-        char keys[10]={'D','B','D','C','C','D','A','E','A','D'};
-            for(i=0; i<8; i++) {
-            	printf("std %d => %d\n", (i+1), checkscore(ans[i], keys));
-            }
-        printf("Number 1 correct stat => %d\n", no1);
-        printf("Hardest question => %d\n", checkhardest(ans,keys));
+int checkNum(int numAr[],int numArSize) {
+    int rnum;
+    rnum=random1to100();
+    while (searchNum(numAr,numArSize,rnum)) {
+        rnum=random1to100();
+    }
+    return rnum;
 }
 
-
-int checkscore(char std[], char keys[]) {
-    int i, scr=0;
-    for(i=0; i<10; i++) {
-        if(std[i] == keys[i]) {
-            scr++;
-            if(i==0) {
-                no1++;
-            }
+int searchNum(int numAr[],int numArSize, int target) {
+    //sol1* use Linear search
+    //sol2 use binary search  see.Lab6no9-10 
+    int i,found=0;
+    for (i=0;i<numArSize;i++) {
+        if(target == numAr[i]){
+            found = 1;
         }
     }
     
-    return scr;
+    return found;
 }
 
-
-int checkhardest(char ans[][10], char keys[]) {
-    int i, j, correct;
-    int lowest = 9, hardest = 1;
-
-    for(i=0;i<10;i++){
-        correct = 0;
-        for(j=0;j<8;j++){
-            if(ans[j][i] == keys[i]){
-                correct++;
-            }
-        }
-        if(correct < lowest){
-            lowest = correct;
-            hardest = i+1;
-        }
-    }
-
-    return hardest;
+int random1to100() {
+    int random_num = (rand() % 100) + 1;
+    printf("%d ", random_num);
+    return random_num;
 }
-
